272eb565a7c676c19abce2b963d1522cd93c8ccf
ronaldcho <ronaldcho@gmail.com>
2018-08-30 10:39:40 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>
+      A self-quick learner, effective communication skills with different levels
+      of tech and non-tech people. Coding JavaScript, React, Nodejs, MangoDB,
+      Python, Django.
+    </p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
